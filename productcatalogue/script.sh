cfcb2b588e205f85d92bc22658b5c96212ad2466
umi2023 <131685697+umi2023@users.noreply.github.com>
2023-06-18 19:01:15 +0100

    Update script.sh

--- productcatalogue/script.sh
+++ productcatalogue/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 /usr/local/bin/node_exporter &
-java -Djava.security.egd=file:/dev/./urandom -jar /app.jar
+java -Djava.security.egd=file:/dev/./urandom -jar /app.jar server /app-config.yml
